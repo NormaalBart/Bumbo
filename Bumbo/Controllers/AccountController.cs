a72cf32fac823b4de0c3de2472eab7b191eefbf1
Cas Denteneer <c.denteneer@student.avans.nl>
2022-11-22 15:44:37 +0100

    Fix voor medewerker kan rooster zien

--- Bumbo/Controllers/AccountController.cs
+++ Bumbo/Controllers/AccountController.cs
@@ -73,7 +73,7 @@ namespace Bumbo.Controllers
             }
             else if (roles.Contains(RoleType.EMPLOYEE.Name))
             {
-                return RedirectToAction("Index", "Employee");
+                return RedirectToAction("Index", "EmployeeRoster");
             }
             return RedirectToAction("Login");
         }
