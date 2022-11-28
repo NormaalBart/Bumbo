7d3c800cd4ef4457998a6bb519d7e0fabdfc29f4
Luuk <luukvink04@gmail.com>
2022-11-28 11:33:06 +0100

    Update EmployeeListIndexViewModel.cs

--- Bumbo/Models/EmployeeManager/Index/EmployeeListIndexViewModel.cs
+++ Bumbo/Models/EmployeeManager/Index/EmployeeListIndexViewModel.cs
@@ -12,7 +12,6 @@ namespace Bumbo.Models.EmployeeManager.Index
         [DisplayName("Toon medewerkers niet in dients")]
         public bool IncludeInactive { get; set; }
         public string SearchString { get; set; }
-        [DisplayName("---")]
         public EmployeeSortingOption CurrentSort { get; set; }
 
         public List<EmployeeSortingOption> AvailableSortOptions { get; set; }
