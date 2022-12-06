569d8a5a28925bb5a9e7e1afc7bf8c11799fff15
NormaalBart <bwj.kempen@student.avans.nl>
2022-12-06 18:09:33 +0100

    Remade notifications

--- Bumbo/wwwroot/js/notification.js
+++ Bumbo/wwwroot/js/notification.js
@@ -1,18 +1,15 @@
 ﻿const Notification = {
-    show(message, state) {
-        clearTimeout(this.hideTimeout);
-
-        this.el = document.createElement("div");
-        this.el.textContent = message;
-        this.el.className = "notification notification--visible";
-        document.body.appendChild(this.el);
-
-        if (state) {
-            this.el.classList.add(`notification--${state}`);
-        }
-
-        this.hideTimeout = setTimeout(() => {
-            this.el.remove();
-        }, 5000);
+    show(type, message) {
+        const Toast = Swal.mixin({
+            toast: true,
+            position: 'top-end',
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+        })
+        Toast.fire({
+            icon: 'success',
+            title: message
+        });
     }
-};
+}
