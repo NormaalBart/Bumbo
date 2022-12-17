b37136e2ee30947a6fa4da91e067dd4fdbd497aa
Luuk <luukvink04@gmail.com>
2022-12-17 16:19:54 +0100

    removed unused code and fixed error

--- Bumbo/Models/EmployeeRoster/EmployeeShiftsListViewModel.cs
+++ Bumbo/Models/EmployeeRoster/EmployeeShiftsListViewModel.cs
@@ -1,7 +1,4 @@
-﻿using System.Globalization;
-using BumboRepositories.Utils;
-
-namespace Bumbo.Models.EmployeeRoster
+﻿namespace Bumbo.Models.EmployeeRoster
 {
     public class EmployeeShiftsListViewModel
     {
@@ -9,50 +6,5 @@ namespace Bumbo.Models.EmployeeRoster
 
         public List<EmployeeShiftViewModel> shifts { get; set; }
 
-        // returns the day name of the number of the week
-        public string GetDay(int dayNumber)
-        {
-            // We add 1 because it defaults to american culture for some reason where it's different.
-            dayNumber++;
-            if(dayNumber == 7)
-            {
-                return "zo";
-            }
-            return CultureInfo.CurrentCulture.DateTimeFormat.GetDayName((DayOfWeek)dayNumber).Substring(0, 2);
-        }
-
-        public bool DayIsSelected(int dayNumber)
-        {
-            dayNumber++;
-            if (dayNumber == 7)
-            {
-                dayNumber = 0;
-            }
-            return ((int)Date.DayOfWeek) == dayNumber;
-        }
-
-        public bool IsToday(int i)
-        {
-            DateTime date = Date.ToDateTime(new TimeOnly(0,0,0)).GetMondayOfTheWeek();
-            date = date.AddDays(i);
-            if(DateTime.Now.Month != Date.Month || DateTime.Now.Year != Date.Year)
-            {
-               return false;
-            }
-            if (date == DateTime.Today.Date)
-            {
-                return true;
-            }
-            return false;
-        }
-
-        public DateTime GetDayDate(int dayNumber)
-        {
-            // returns the date of the specific DayOfTheWeek number
-            DateTime monday = Date.ToDateTime(new TimeOnly(0,0,0)).GetMondayOfTheWeek();
-            return monday.AddDays(dayNumber);
-        }
-
-        
     }
 }
