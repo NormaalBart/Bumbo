46ae9f90346e5a83cfcaf0bb2cd8e3bd6cfabffe
Luuk <luukvink04@gmail.com>
2022-12-19 14:22:00 +0100

    restored day navigation

--- Bumbo/Models/EmployeeRoster/EmployeeShiftsListViewModel.cs
+++ Bumbo/Models/EmployeeRoster/EmployeeShiftsListViewModel.cs
@@ -1,4 +1,7 @@
-﻿namespace Bumbo.Models.EmployeeRoster
+﻿using BumboRepositories.Utils;
+using System.Globalization;
+
+namespace Bumbo.Models.EmployeeRoster
 {
     public class EmployeeShiftsListViewModel
     {
@@ -6,5 +9,47 @@
 
         public List<EmployeeShiftViewModel> shifts { get; set; }
 
+        public string GetDay(int dayNumber)
+        {
+            // We add 1 because it defaults to american culture for some reason where it's different.
+            dayNumber++;
+            if (dayNumber == 7)
+            {
+                return "zo";
+            }
+            return CultureInfo.CurrentCulture.DateTimeFormat.GetDayName((DayOfWeek)dayNumber).Substring(0, 2);
+        }
+
+        public bool DayIsSelected(int dayNumber)
+        {
+            dayNumber++;
+            if (dayNumber == 7)
+            {
+                dayNumber = 0;
+            }
+            return ((int)Date.DayOfWeek) == dayNumber;
+        }
+
+        public bool IsToday(int i)
+        {
+            DateTime date = Date.ToDateTime(new TimeOnly(0, 0, 0)).GetMondayOfTheWeek();
+            date = date.AddDays(i);
+            if (DateTime.Now.Month != Date.Month || DateTime.Now.Year != Date.Year)
+            {
+                return false;
+            }
+            if (date == DateTime.Today.Date)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public DateTime GetDayDate(int dayNumber)
+        {
+            // returns the date of the specific DayOfTheWeek number
+            DateTime monday = Date.ToDateTime(new TimeOnly(0, 0, 0)).GetMondayOfTheWeek();
+            return monday.AddDays(dayNumber);
+        }
     }
 }
